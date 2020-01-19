37d81719753926cf218470ae3a93a69b33c7f90c
Pavel Meyerovich <pavel.meyerovich@itechart-group.com>
2020-01-19 11:58:04 +0300

    something like this

--- src/TableOfGame.jsx
+++ src/TableOfGame.jsx
@@ -2,55 +2,44 @@ import React from 'react';
 import {Cube} from './Cube.jsx';
 import {AddForm} from './AddForm.jsx';
 
-function randomColor(){
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-function addCube(
-  cubes,
-  cubeToAdd
-) {
-  // console.log(this.state.cubes);
-  return [...cubes,cubeToAdd]
-  
+function randomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
 }
 
 export class TableOfGame extends React.Component {
 
-  state={
-    cubes: []
-  }
+    state = {
+        cubes: []
+    }
 
-  nextId = 1;
+    nextId = 1;
 
-  render(){
-    return (
-      <>
-      < AddForm
-        onSave={number => {
-          for (let i=0; i<=number; i++){
-            const cube = {
-              id: this.nextId,
-              color: randomColor()
-            };
-
-            this.setState({
-              cubes: addCube(this.state.cubes, cube)
-            });
-
-            this.nextId++;
-          }}
-        }
-      />
-      <div className="table-of-game">
-        {this.state.cubes.map(item => <Cube key={item.id} color={item.color}/>)}
-      </div>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                < AddForm
+                    onSave={number => {
+                        const cubesToAdd = [];
+                        for (let i = 0; i < number; i++) {
+                            cubesToAdd.push({
+                                id: this.nextId,
+                                color: randomColor()
+                            });
+                            this.nextId++;
+                        }
+                        this.setState({cubes: [...this.state.cubes, ...cubesToAdd]});
+                    }
+                    }
+                />
+                <div className="table-of-game">
+                    {this.state.cubes.map(item => <Cube key={item.id} color={item.color}/>)}
+                </div>
+            </>
+        );
+    }
 }
